d5a8c3735907d781c5abba9d3fa01d10ed83f948
chaman3s <chamanggarwal@gmail.com>
2026-03-02 20:13:38 +0530

    fix module issue

--- app/(user)/(main)/home/dashBoard/patientDetail/page.tsx
+++ app/(user)/(main)/home/dashBoard/patientDetail/page.tsx
@@ -1,4 +1,4 @@
-import PatientDetailPage from "@/Screen/main/PatientDetailPage";
+import PatientDetailPage from "@/Screen/user/main/PatientDetailPage";
 
 export default function PatientDetailRoutePage() {
   return <PatientDetailPage />;
